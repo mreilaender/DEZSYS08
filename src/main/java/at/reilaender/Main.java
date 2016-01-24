3635786962e7d2189d2143ea9a1224da7a5d0908
BuildTools <mreilaender@student.tgm.ac.at>
2016-01-24 17:05:31 +0100

    test

--- src/main/java/at/reilaender/Main.java
+++ src/main/java/at/reilaender/Main.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.nio.ByteOrder;
 
 import static org.bridj.Pointer.allocateFloats;
+import static org.bridj.Pointer.allocateInts;
 
 /**
  * @author manue
@@ -75,7 +76,53 @@ public class Main {
             System.out.println(String.format("[%d] Found device: %s", i, devices[i]));
         }
     }
+    public static void test3() {
+        CLContext context = JavaCL.createBestContext();
+        CLQueue queue = context.createDefaultQueue();
+        ByteOrder byteOrder = context.getByteOrder();
+
+        int n = 1024;
+        Pointer<Integer>
+                aPtr = allocateInts(n).order(byteOrder),
+                bPtr = allocateInts(n).order(byteOrder);
+
+        aPtr.set(0, 1);
+        bPtr.set(0, 100);
+
+        // Create OpenCL input buffers (using the native memory pointers aPtr and bPtr) :
+        CLBuffer<Integer>
+                a = context.createBuffer(CLMem.Usage.Input, aPtr),
+                b = context.createBuffer(CLMem.Usage.Input, bPtr);
+
+        // Create an OpenCL output buffer :
+        /* TODO made by me */
+        final Pointer<Float> nPtr = Pointer.allocateFloats(n).order(byteOrder);
+        CLBuffer<Float> out = context.createBuffer(CLMem.Usage.Output, nPtr);
+
+        // Read the program sources and compile them :
+        /* Get resource file */
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        //InputStream is = classloader.getResourceAsStream("TutorialKernels.cl");
+        String src = null;
+        try {
+            src = IOUtils.readText(classloader.getResource("opencl.cl"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        CLProgram program = context.createProgram(src);
+
+        // Get and call the kernel :
+        CLKernel addFloatsKernel = program.createKernel("calculate_pi");
+        addFloatsKernel.setArgs(a, b, out, n);
+        CLEvent addEvt = addFloatsKernel.enqueueNDRange(queue, new int[] { n });
+
+        Pointer<Float> outPtr = out.read(queue, addEvt); // blocks until add_floats finished
+
+        // Print the first 10 output values :
+        for (int i = 0; i < 10 && i < n; i++)
+            System.out.println("out[" + i + "] = " + outPtr.get(i));
+    }
     public static void main(String[] args) {
-        test2();
+        test3();
     }
 }
--- src/main/resources/opencl.cl
+++ src/main/resources/opencl.cl
@@ -6,3 +6,9 @@ __kernel void add_floats(__global const float* a, __global const float* b, __glo
 
     out[i] = a[i] + b[i];
 }
+__kernel void calculate_pi(__global const int* start, __global const int* end, __global float* out, int n)
+{
+    int i;
+    printf("Start: %d, End: %d", start[0], end[0]);
+    //for(i=start; i < *end)
+}
